47c113639548bcde6e53d6decd1d785b7411e9dd
miladmirzaei <miladmirzaei777@gmail.com>
2025-03-15 01:58:26 +0330

    s

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <>
+  <div className="flex items-center justfy-center flex-col">
   
-  <div className="logo flex items-center"><b> MR <span> BtC </span>Gambler</b></div>
+  <div className="logo"><b> MR <span> BtC </span>Gambler</b></div>
 
   <h1><span>I</span>M<span>POSSIBLE</span></h1>
+  <a href="#" className="neon-button absolute bottom-0">Discord</a>
 <a href="https://x.com/mrbtcgambler" id="btn-twtr" target="_b">Follow me on X</a>
-  </>
+  </div>
   );
 }
