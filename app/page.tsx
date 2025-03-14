b5e3b53f12d45c7806e51a388eb5b17798b6a4dc
miladmirzaei <miladmirzaei777@gmail.com>
2025-03-15 01:47:17 +0330

    aa

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
   <>
   
-  <div className="logo"><b>MR<span> BtC</span>Gambler</b></div>
+  <div className="logo flex items-center"><b> MR <span> BtC </span>Gambler</b></div>
 
-
+  <h1><span>I</span>M<span>POSSIBLE</span></h1>
 <a href="https://x.com/mrbtcgambler" id="btn-twtr" target="_b">Follow me on X</a>
   </>
   );
