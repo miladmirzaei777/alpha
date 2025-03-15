27f47f2ed27de99c2090ed6f9a6ecc45d0c5595a
miladmirzaei <miladmirzaei777@gmail.com>
2025-03-15 10:44:48 +0330

    new

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,32 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <div className="flex items-center justfy-center flex-col">
-  
-  <div className="logo"><b> MR <span> BtC </span>Gambler</b></div>
+    <div className="flex flex-col gap-2 overflow-hidden items-center justify-center min-h-screen">
+      <div>
 
-  <h1><span>I</span>M<span>POSSIBLE</span></h1>
-  <a href="#" className="neon-button absolute bottom-0">Discord</a>
-<a href="https://x.com/mrbtcgambler" id="btn-twtr" target="_b">Follow me on X</a>
-  </div>
+      <div className="logo text-3xl lg:text-5xl flex">
+        <b>
+          {" "}
+          MR <span> BtC </span>Gambler
+        </b>
+      </div>
+      </div>
+
+      <h1>
+        <span>I</span>M<span>POSSIBLE</span>
+      </h1>
+      <div className="flex gap-2">
+      <a href="#" className="neon-button absolute bottom-0">
+        Discord
+      </a>
+      <a href="#" className="neon-button absolute bottom-0">
+        Youtube
+      </a>
+      
+      </div>
+      <a className="w-fit" href="https://x.com/mrbtcgambler" id="btn-twtr" target="_b">
+        Follow me on X
+      </a>
+    </div>
   );
 }
