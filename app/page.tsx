e7ecf0f865c088230826a7e4a6646e4d3f8774af
miladmirzaei <miladmirzaei777@gmail.com>
2025-03-24 01:10:01 +0330

    ggggg

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,13 @@ import Image from "next/image";
 import { createOrUpdateUser } from "@/lib/actions/user";
 import { connect } from '@/lib/mongodb/mongoose';
 export default async function Home() {
-
+  // const user = await createOrUpdateUser(
+  //   "1111121",
+  //   "first_name",
+  //   "last_name",
+  //   "image_url",
+  //   "email_addresses"
+  // );
   return (
     <>
       Hello world
