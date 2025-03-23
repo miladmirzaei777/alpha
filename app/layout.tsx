2eff2c4b28ac46a597de98fb1d21417291e270f9
miladmirzaei <miladmirzaei777@gmail.com>
2025-03-24 01:10:31 +0330

    ggggg

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <header className="flex justify-end items-center p-4 gap-4 h-16 bg-red-400">
+          <header className="flex justify-end items-center p-4 gap-4 h-16 bg-green-400">
             <div className='cursor-pointer'>
             <SignedOut>
               <SignInButton />
